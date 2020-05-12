a96b113c06920193b7496cdb6dc3754f941ad0d9
kousigan <kousiunleashed@gmail.com>
2020-05-12 19:58:52 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Import stylesheets
 import './style.css';
 import './tab.css';
-
+import './gridContainer.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 // appDiv.innerHTML = `<h1>JS Starter</h1>`;
